c9331b811e229544b0a2286cbb66cadf33272068
ahmed dacharoui <dachraouia193@gmail.com>
2025-02-22 18:14:51 +0100

    first commit

---
+++ src/main/java/com/rh_app/hr_app/HrAppApplication.java
@@ -0,0 +1,13 @@
+package com.rh_app.hr_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrAppApplication.class, args);
+	}
+
+}
