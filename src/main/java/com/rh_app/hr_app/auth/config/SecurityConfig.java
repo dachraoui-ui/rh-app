eb49b1ec96053fa644472bf138b33dff59970ac9
Ben othman Rayen <raydineri07@gmail.com>
2025-02-25 13:26:21 +0100

    back-end-login

---
+++ src/main/java/com/rh_app/hr_app/auth/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.rh_app.hr_app.auth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/api/drh/**").hasRole("DRH")
+                        .anyRequest().authenticated()
+                )
+                .oauth2Login(); // Pour Keycloak OAuth2
+
+        return http.build();
+    }
+}
+
+
---
+++ src/main/java/com/rh_app/hr_app/auth/controller/AuthController.java
@@ -0,0 +1,4 @@
+package com.rh_app.hr_app.auth.controller;
+
+public class AuthController {
+}
