760bd14b2a8db6b4a10db15228ca753083df9567
ahmed dacharoui <dachraouia193@gmail.com>
2025-03-13 11:08:28 +0100

    fix keycloak config files

--- src/main/java/com/rh_app/hr_app/core/security/SecurityConfig.java
+++ src/main/java/com/rh_app/hr_app/core/security/SecurityConfig.java
@@ -1,35 +1,59 @@
-package com.rh_app.hr_app.core.security;
+package com.example.book_social_network.security;
 
+import com.rh_app.hr_app.core.security.KeycloakJwtAuthenticationConverter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+
+@Configuration//configuration class
 @EnableWebSecurity
+@RequiredArgsConstructor
+@EnableMethodSecurity(securedEnabled = true)
 public class SecurityConfig {
 
-    @Bean
+
+
+    @Bean //method that returns an object that should be registered as a bean in the Spring application context
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
         http
-                .authorizeHttpRequests(authorizeRequests ->
-                        authorizeRequests
-                                .requestMatchers("/", "/error").permitAll()
-                                .anyRequest().authenticated()
+                .cors(withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(req ->
+                        req.requestMatchers(
+                                        "/auth/**",
+                                        "/v2/api-docs",
+                                        "/v3/api-docs",
+                                        "/v3/api-docs/**",
+                                        "/swagger-resources",
+                                        "/swagger-resources/**",
+                                        "/configuration/ui",
+                                        "/configuration/security",
+                                        "/swagger-ui/**",
+                                        "/webjars/**",
+                                        "/swagger-ui.html"
+                                )
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated()
                 )
-                .oauth2Login(oauth2 -> oauth2
-                        .defaultSuccessUrl("/home", true)
-                        .failureUrl("/error")
-                )
-                .logout(logout -> logout
-                        .logoutSuccessUrl("/")
-                        .invalidateHttpSession(true)
-                );
+                .oauth2ResourceServer(auth ->
+                        auth.jwt(token
+                                -> token.jwtAuthenticationConverter(new KeycloakJwtAuthenticationConverter())));
+
+
 
         return http.build();
     }
 }
-
-
 
