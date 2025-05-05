e5838cdf941a7476792ddab1e39196a3ccff0a0f
Ben othman Rayen <raydineri07@gmail.com>
2025-05-05 15:19:25 +0100

    adding islamic-Holidays for the calendar

---
+++ src/main/java/com/rh_app/hr_app/features/holidays/mapper/IslamicHolidayMapper.java
@@ -0,0 +1,28 @@
+package com.rh_app.hr_app.features.holidays.mapper;
+
+import com.rh_app.hr_app.features.holidays.dto.IslamicHolidayDto;
+import com.rh_app.hr_app.features.holidays.model.IslamicHoliday;
+
+public class IslamicHolidayMapper {
+    public static IslamicHolidayDto toDto(IslamicHoliday holiday) {
+        return IslamicHolidayDto.builder()
+                .id(holiday.getId())
+                .name(holiday.getName())
+                .date(holiday.getDate())
+                .description(holiday.getDescription())
+                .year(holiday.getYear())
+                .durationDays(holiday.getDurationDays())
+                .build();
+    }
+
+    public static IslamicHoliday toEntity(IslamicHolidayDto dto) {
+        return IslamicHoliday.builder()
+                .id(dto.getId())
+                .name(dto.getName())
+                .date(dto.getDate())
+                .description(dto.getDescription())
+                .year(dto.getYear())
+                .durationDays(dto.getDurationDays())
+                .build();
+    }
+}
